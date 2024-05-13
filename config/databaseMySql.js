d72a5fd012d6370d7d99a6935d21173e3c316672
akshatchawla1997 <akshatchawla1997@gmail.com>
2024-05-13 16:29:09 +0530

    Update databaseMySql.js

--- config/databaseMySql.js
+++ config/databaseMySql.js
@@ -2,11 +2,22 @@ const mysql = require("mysql2")
 // const {DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE} = require("../../env")
 const pool = mysql.createPool({
     connectionLimit : 10,
-    host: "localhost",  // ip address of server running mysql
+    // host: "185.80.128.192",  // ip address of server running mysql
+    // port:  "3306",
+    // user: "nhg",    // user name to your mysql database
+    // password: "yDw57p09!",
+    // database: "nhg", // use the specified database
+
+    host: "127.0.0.1",  // ip address of server running mysql
     port:  "3306",
-    user: "nhg",    // user name to your mysql database
-    password: 'yDw57p09!',
-    database: "nhg", // use the specified database
+    user: "root",    // user name to your mysql database
+    password: "1234",
+    database: "nhg_finance", // use the specified database
+//     DB_HOST=127.0.0.1
+// DB_USER=root
+// DB_PASSWORD=1234
+// DB_DATABASE=ngh_finance
+// MYSQLPORT=3306
     multipleStatements: true
 })
 
